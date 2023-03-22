a0066da966f9b6ebb4e91828b2c3707d38874ef9
jugomezcr <jugomezcr@unal.edu.co>
2023-03-22 11:56:24 -0500

    Applying repulsion and attraction forces

--- ParticleSystem/src/ParticlesApp.cpp
+++ ParticleSystem/src/ParticlesApp.cpp
@@ -21,11 +21,16 @@ class ParticlesApp : public App {
 	    void update();
 	    void draw();
         void keyDown( KeyEvent event ); //Implementacion inputs de teclado.
+        void mouseMove(MouseEvent event);
+        void mouseDown(MouseEvent event);
     
     //Variables y objetos(miembros) publicos.
     public:
         ParticleSystem mParticleSystem; //Se declara un objeto(member) de la clase ParticleSystem con el nombre de mParticleSystem.
-    
+
+        //Aplicando fuerzas de repulsion y atraccion.
+        vec2 attrPosition;
+        float attrFactor, repulsionFactor, repulsionRadius;
 };
 
 void ParticlesApp::setup(){
@@ -41,9 +46,21 @@ void ParticlesApp::setup(){
         Particle *particle = new Particle( vec2( x, y ), radius, mass, drag );
         mParticleSystem.addParticle( particle );
     }
+
+    //Inicializando las fuerzas de repulsion y atraccion.
+    attrPosition = getWindowCenter();
+    attrFactor = 0.000005f;
+    repulsionRadius = 5.f;
+    repulsionFactor = -0.0005f;
 }
 
 void ParticlesApp::update(){
+    for(std::vector<Particle*>::iterator it = mParticleSystem.particles.begin(); it != mParticleSystem.particles.end(); ++it ){
+        vec2 attrForce = attrPosition - (*it)->position;
+        attrForce *= attrFactor;
+        (*it)->forces += attrForce;
+    }
+
     mParticleSystem.update();
 }
 
@@ -54,11 +71,26 @@ void ParticlesApp::draw(){
 }
 
 //Eventos del teclado.
-void ParticlesApp::keyDown( KeyEvent event )
+void ParticlesApp::keyDown(KeyEvent event )
 {
 	if( event.getCode() == KeyEvent::KEY_ESCAPE ) {
 			quit();
 	}
+}
+
+//Eventos de mouse.
+void ParticlesApp::mouseMove(MouseEvent event){
+    attrPosition.x = event.getPos().x;
+    attrPosition.y = event.getPos().y;
+}
+
+void ParticlesApp::mouseDown(MouseEvent event){
+    for( std::vector<Particle*>::iterator it = mParticleSystem.particles.begin(); it != mParticleSystem.particles.end(); ++it ) {
+        //vec2 repulsionForce = (*it)->position - glm::intBitsToFloat(event.getPos()); //Genera error de type, por eso se coloca una conversion.
+        vec2 repulsionForce = (*it)->position - vec2(float(event.getPos().x), float(event.getPos().y));
+        repulsionForce = glm::normalize(repulsionForce) * math<float>::max(0.f, repulsionRadius - repulsionForce.length());
+        (*it)->forces += repulsionForce;
+    }
 }
 
 CINDER_APP(ParticlesApp, RendererGl)
