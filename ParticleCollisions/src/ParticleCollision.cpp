702da8c12538250538785ba30be67c2ae4d45f2e
Diego Mejia <64498344+dimejia144@users.noreply.github.com>
2023-03-25 17:26:32 -0500

    Update ParticleCollision.cpp

--- ParticleCollisions/src/ParticleCollision.cpp
+++ ParticleCollisions/src/ParticleCollision.cpp
@@ -3,6 +3,7 @@
 #include "cinder/Rand.h"
 #include "cinder/gl/gl.h"
 #include <vector>
+#include "cinder/params/Params.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -19,6 +20,9 @@ class Particle {
         vec2 acceleration; // Aceleración actual de la partícula
         vec2 force; // Fuerza resultante que actúa sobre la partícula
         ColorA color;
+    float gravity;
+        bool Opcion;
+        int NP;
 };
 
 class ParticleCollision : public App {
@@ -28,10 +32,25 @@ class ParticleCollision : public App {
         void draw() override;
     private:
         vector<Particle> particles;
+    params::InterfaceGlRef mParams;
 };
 
 void ParticleCollision::setup() {
+    
+    // Crear el GUI con los controles
+    mParams = params::InterfaceGl::create(getWindow(), "La particula", toPixels(ivec2(200, 400)));
+    mParams.addParam("Activar modo random", &Opcion);
+    mParams.addText("Si su respuesta es negativa, por favot no rellene los espacios", "font-size=24");
+    mParams.addSeparator(); // add horizontal line separating controls
+    mParams.addParam("Radio Particula", &radius, "min=5.0 max=20.0 step = 0.5 keyIncr = z keyDecr = Z"); //Radio
+    mParams.addParam("Masa Particula", &mass, "min=5.0 max=20.0 step = 0.5 keyIncr = z keyDecr = Z"); // Masa
+    mParams.addParam("Gravedad del sistema", &gravity, "min=2 max=15 step = 0.1 keyIncr = z keyDecr = Z"); // Gravedad
+    mParams.addSeparator(); // add horizontal line separating controls
+    mParams.addParam("Velocidad", &velocity, "") // Velocidad
+        mParams.addParam("Numero de particulas" & NP, "min=1 max=50 step = 5 keyIncr = z keyDecr = Z");
+    
     // Inicializar las partículas
+     if (Opcion) {
     for (int i = 0; i < NUM_PARTICLES; i++) {
         Particle p;
         // Configurar valores iniciales
@@ -44,6 +63,21 @@ void ParticleCollision::setup() {
         // Asigna un color random
         p.color = Color( CM_HSV, lmap<float>( i, 0.0f, NUM_PARTICLES, 0.0f, 0.66f ), 1.0f, 1.0f );
         particles.push_back(p);
+    }
+     }
+    for (int i = 0; i < NP; i++) {
+        Particle p;
+        // Configurar valores iniciales
+        p.position = vec2(Rand::randFloat(getWindowWidth()), Rand::randFloat(getWindowHeight()));
+        p.velocity = vec2(Rand::randFloat(-5.0f, 5.0f), Rand::randFloat(-5.0f, 5.0f));
+        p.radius = radius;
+        p.mass = mass;
+        p.acceleration = vec2(0, 0);
+        p.force = vec2(0, 0);
+        // Asigna un color random
+        p.color = Color(CM_HSV, lmap<float>(i, 0.0f, NUM_PARTICLES, 0.0f, 0.66f), 1.0f, 1.0f);
+        particles.push_back(p);
+        //Tocaria agregarle lo que hicieron los demas de la velocidad cambiante o de la gravedad pero no se como ponerlo :v
     }
 }
 
